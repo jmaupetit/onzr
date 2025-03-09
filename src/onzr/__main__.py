c11b9af9be13c9a09acb78aaff43dc1c60b0d50d
Julien Maupetit <julien@maupetit.net>
2025-03-09 22:16:18 +0100

    ✨(cli) add base cli with search/play commands
    Add base CLI with two search and play commands. This is a draft first
    implementation.

--- src/onzr/__main__.py
+++ src/onzr/__main__.py
@@ -1,13 +1,6 @@
 """Onzr."""
 
-import logging
+from .cli import cli
 
-from .core import Onzr
-from .deezer import StreamQuality
-
-logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.DEBUG)
-
-track_id = "2107136627"
-onzr = Onzr()
-onzr.play(track_id, StreamQuality.MP3_128)
+if __name__ == "__main__":
+    cli()
