9b0fb840932e45b5eed385a91f03d2dd1c734e2e
Julien Maupetit <julien@maupetit.net>
2025-06-19 23:52:53 +0200

    Remove commented code

--- src/onzr/config.py
+++ src/onzr/config.py
@@ -26,30 +26,6 @@ def get_onzr_dir() -> Path:
     return Path(get_app_dir(APP_NAME))
 
 
-# def get_settings() -> Dynaconf:
-#     """Get Dynaconf settiings."""
-#     logger.debug("Getting settings…")
-#     settings = Dynaconf(
-#         envvar_prefix=APP_NAME.upper(),
-#         root_path=get_onzr_dir(),
-#         settings_files=[SECRETS_FILE.name, SETTINGS_FILE.name],
-#         validators=[
-#             Validator("ARL", must_exist=True),
-#             Validator("DEEZER_BLOWFISH_SECRET", must_exist=True),
-#             Validator("QUALITY", must_exist=True, cast=StreamQuality),
-#         ],
-#     )
-#
-#     # Check settings
-#     try:
-#         settings.validators.validate_all()
-#     except ValidationError as err:
-#         mess = "Onzr is not properly configured:\n"
-#         mess += "\n".join([f"- {e[1]}" for e in err.details])
-#         raise OnzrConfigurationError(mess) from err
-#     return settings
-
-
 class Settings(BaseSettings):
     """Onzr application settings."""
 
