d4b28d78176ac6fd4439c973e63b5ea9813cc4f9
Julien Maupetit <julien@maupetit.net>
2025-11-13 09:26:09 +0100

    ⚡️(deezer) parallelize track info fetching
    Since we make an extra request to the Deezer API to get each track
    release date, the CLI performances decreased since we sequentially
    update track information.
    
    We propose to speed up the process by running each request in a
    concurrent thread.

--- tests/factories.py
+++ tests/factories.py
@@ -1,12 +1,22 @@
 """Test factories."""
 
 from polyfactory.factories.pydantic_factory import ModelFactory
+from pydantic import PositiveInt
 
-from .models import DeezerSong, DeezerSongResponse
+from .models import DeezerSong, DeezerSongResponse, DeezerTrack
 
 
 class DeezerSongFactory(ModelFactory[DeezerSong]):
     """DeezerSong factory."""
+
+    @classmethod
+    def FILESIZE_MP3_128(cls) -> PositiveInt:
+        """Force FILESIZE_MP3_128 to be at least 100."""
+        return cls.__random__.randint(100, 10000)
+
+
+class DeezerTrackFactory(ModelFactory[DeezerTrack]):
+    """DeezerTrack factory."""
 
 
 class DeezerSongResponseFactory(ModelFactory[DeezerSongResponse]):
