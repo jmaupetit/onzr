8e8d669e4c99060901ad548757b1e9cc70391f18
Thierry Chantier <titimoby@gmail.com>
2025-11-17 20:57:38 +0100

    🚸(tui) fix linter issues

--- src/onzr/tui.py
+++ src/onzr/tui.py
@@ -1,14 +1,25 @@
+"""Onzr: TUI elements."""
+
 from textual.app import App, ComposeResult
-from textual.containers import Vertical, Container, HorizontalGroup
+from textual.containers import Container, HorizontalGroup, Vertical
 from textual.reactive import reactive
-from textual.widgets import Footer, TabbedContent, TabPane, Static, Button, DataTable, Header
+from textual.widgets import (
+    Button,
+    DataTable,
+    Footer,
+    Header,
+    Static,
+    TabbedContent,
+    TabPane,
+)
 
 from onzr import cli
 from onzr.client import OnzrClient
-from onzr.models import QueuedTracks, PlayingState
+from onzr.models import PlayingState, QueuedTracks
 
 
 def get_now_playing():
+    """Get the current playing status."""
     client = OnzrClient()
     playing_state: PlayingState = client.now_playing()
     if playing_state.track is not None:
@@ -16,17 +27,20 @@ def get_now_playing():
     else:
         return "Nothing"
 
+
 class PlayStatusWidget(Static):
     """Show the current playing status."""
 
     now_playing_text = reactive("Nothing", layout=True)
 
     def render(self) -> str:
+        """Render the current playing status."""
         return f"Now playing: {self.now_playing_text}!"
 
 
 class PlayControl(HorizontalGroup):
     """A group of buttons to control play/pause/stop music."""
+
     now_playing_text = reactive("Nothing", layout=True)
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
@@ -50,24 +64,36 @@ class PlayControl(HorizontalGroup):
         yield Button("Play", id="play", variant="primary", classes="play-button")
         yield Button("Pause", id="pause", variant="primary", classes="play-button")
         yield Button("Stop", id="stop", variant="primary", classes="play-button")
-        yield Button("Previous", id="previous", variant="primary", classes="play-button")
+        yield Button(
+            "Previous", id="previous", variant="primary", classes="play-button"
+        )
         yield Button("Next", id="next", variant="primary", classes="play-button")
 
 
 class OnzrTuiApp(App):
+    """OnzrTui app.
+
+    This is the class that will:
+    - define the TUI, compose the interface with the widgets
+    - gather the information about the playlist
+    - allow features such as search...
+    """
+
     CSS_PATH = "tui.tcss"
 
     BINDINGS = [
         ("p", "show_tab('playlist-tab')", "Play list"),
         ("s", "show_tab('search-tab')", "Search"),
-        ("ctrl+q", "quit", "Quit")
+        ("ctrl+q", "quit", "Quit"),
     ]
 
     def on_mount(self) -> None:
+        """Initialize the app."""
         self.title = "ONZR: play your music from the terminal"
         self.set_interval(1, self.update_status)
 
     def update_status(self):
+        """Update the text displayed in the status widget."""
         self.query_one(PlayStatusWidget).now_playing_text = get_now_playing()
 
     def compose(self) -> ComposeResult:
@@ -86,9 +112,10 @@ class OnzrTuiApp(App):
         yield Footer()
 
     def get_playlist_items(self) -> DataTable:
+        """Create the playlist items table."""
         client = OnzrClient()
         queue: QueuedTracks = client.queue_list()
-        table = DataTable()
+        table: DataTable = DataTable()
 
         table.add_column("ID")
         table.add_column("Title")
@@ -96,6 +123,11 @@ class OnzrTuiApp(App):
         table.add_column("Album")
 
         for qtrack in queue.tracks:
-            table.add_row(qtrack.track.id, qtrack.track.title, qtrack.track.artist, qtrack.track.album)
+            table.add_row(
+                qtrack.track.id,
+                qtrack.track.title,
+                qtrack.track.artist,
+                qtrack.track.album,
+            )
 
         return table
