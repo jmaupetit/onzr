ee3dd582b1eb4ec8088c8a84a4c28dd88aee77c1
Julien Maupetit <julien@maupetit.net>
2025-11-27 17:23:41 +0100

    add always fetch release config

--- src/onzr/cli.py
+++ src/onzr/cli.py
@@ -49,7 +49,7 @@ from .models import (
 FORMAT = "%(message)s"
 logging_console = Console(stderr=True)
 logging_config = {
-    "level": logging.INFO,
+    "level": logging.DEBUG,
     "format": FORMAT,
     "datefmt": "[%X]",
     "handlers": [RichHandler(console=logging_console)],
@@ -90,6 +90,7 @@ def get_deezer_client(quiet: bool = False) -> DeezerClient:
         blowfish=settings.DEEZER_BLOWFISH_SECRET,
         fast=True,
         connection_pool_maxsize=settings.CONNECTION_POOL_MAXSIZE,
+        always_fetch_release_date=settings.ALWAYS_FETCH_RELEASE_DATE,
     )
 
 
